30947894f9d24c63d7924125dc846ce44401ae2b
evgenii_mizulin <evgeny.mizulin@gmail.com>
2019-07-12 17:19:28 +0300

    no magic strings in projects

--- test/projects/post-projects.test.js
+++ test/projects/post-projects.test.js
@@ -5,9 +5,11 @@ const { app } = require('../../src/index')
 const assert = require('assert')
 const { Project, ProjectImage, ProjectPermission } = require('../../src/services/db/Db')
 const { getAuth } = require('../helpers/getAuth')
+const randomstring = require('randomstring')
 
 let auth
-let project
+const sendedProject = { name: randomstring.generate() }
+let createdProject
 
 describe('POST /projects', () => {
   before(async () => {
@@ -18,14 +20,14 @@ describe('POST /projects', () => {
     request(app)
       .post('/projects')
       .set('AccessToken', auth.accessToken.token)
-      .send({ name: 'post-projects-success' })
+      .send({ name: sendedProject.name })
       .expect(200)
       .expect(res => {
         const { id, name, ...rest } = res.body
-        assert.equal(name, 'post-projects-success')
+        assert.equal(name, sendedProject.name)
         assert.equal(id.length, 24)
         assert.deepEqual(rest, {})
-        project = res.body
+        createdProject = res.body
       })
       .end(done)
   })
@@ -40,27 +42,45 @@ describe('POST /projects', () => {
   })
 
   after(async () => {
-    await (async () => {
-      const images = await ProjectImage.find({ projectId: project.id })
-      const { id, projectId, buffer, ...rest } = images[0]
-      assert.equal(images.length, 1)
-      assert.equal(id.toString().length, 24)
+    const { project, projectImage, projectPermission } = await (async () => {
+      const project = await Project.findOne({ name: sendedProject.name })
+      const projectImage = await ProjectImage.findOne({ projectId: project.id })
+      const projectPermission = await ProjectPermission.findOne({ clientId: auth.client.id })
+      return { project, projectImage, projectPermission }
+    })()
+
+    await Project.remove(project.id)
+    await ProjectImage.remove(projectImage.id)
+    await ProjectPermission.remove(projectPermission.id)
+    await auth.remove()
+
+    {
+      const { id, name, ...rest } = project
+      assert.equal(id.toString(), createdProject.id)
+      assert.deepEqual(rest, {})
+    }
+
+    {
+      const { id, projectId, buffer, ...rest } = projectImage
       assert.equal(buffer instanceof Buffer, true)
       assert.deepEqual(rest, {})
-      await ProjectImage.remove(id)
-    })()
-    await (async () => {
-      const permissions = await ProjectPermission.find({ clientId: auth.client.id })
-      const { id, projectId, clientId, projectRead, projectUpdate, projectDelete, ...rest } = permissions[0]
+    }
+
+    {
+      const {
+        id,
+        projectId,
+        clientId,
+        projectRead,
+        projectUpdate,
+        projectDelete,
+        ...rest
+      } = projectPermission
       assert.equal(projectId.toString(), project.id)
       assert.equal(projectRead, true)
       assert.equal(projectUpdate, true)
       assert.equal(projectDelete, true)
       assert.deepEqual(rest, {})
-      await ProjectPermission.remove(id)
-    })()
-
-    await Project.remove(project.id)
-    await auth.remove()
+    }
   })
 })
